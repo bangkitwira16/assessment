e676c97bd0af103048b350d4e36f3b17b03ebf19
bangkitwira <bangkitwira@gmail.com>
2024-03-12 18:17:06 +0700

    feat: implements all questions into app components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Question1Component } from './components/question-1/question-1.component';
+import { Question2Component } from './components/question-2/question-2.component';
+import { Question3Component } from './components/question-3/question-3.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
+    Question1Component,
+    Question2Component,
+    Question3Component,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
